edb4c4cdae83eef376c443c5c9b945220ac3f274
RikiReal <rijad@tutanota.de>
2025-11-30 21:33:38 +0100

    Add software

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,21 +4,63 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf5 -y copr enable solopasha/hyprland
+dnf5 -y copr enable erikreider/SwayNotificationCenter
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+### Installing hyprland specific packages
+dnf5 -y install		\
+	hyprland	\
+	hyprlock	\
+	hypridle	\
+	hyprpaper	\
+	hyprcursor	\
+	hyprsunset	\
+	xdg-desktop-portal-hyprland	\
+	xdg-desktop-portal-gtk		\
+	hyperpolkitagent		\
+	SwayNotificationCenter
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable solopasha/hyprland
+dnf5 -y copr disable erikreider/SwayNotificationCenter
 
-#### Example for enabling a System Unit File
+### Installing system utilities
+# Is pre-enabled
+# dnf5 -y copr enable che/nerd-fonts
+dnf5 -y install		\
+	nerd-fonts	\
+	waybar		\
+	rofi		\
+	qt5-wayland	\
+	qt6-wayland	\
+	wl-clipboard	\
+	wlogout		\
+	pipewire	\
+	wireplumber	\
+	playerctl	\
+	ddcutil		\
+	network-manager-applet \
+	uswm		\
+	sddm		\
+	grim		\
+	slurp		\
+	swappy		\
+	gnome_keyring	\
+	udiskie		\
+	wob
 
-systemctl enable podman.socket
+### Installing user apps 
+dnf5 copr enable atim/starship
+
+dnf5 -y install 	\
+	timg		\
+	btop		\
+	kitty		\
+	fish		\
+	starship	\
+	nautilus	\
+	fastfetch	\
+	Kvantum
+
+dnf5 copr disable atim/starship
+
+### Add binaries of zed/code, superfile, brave browser, yadm? directly to image using their binaries
